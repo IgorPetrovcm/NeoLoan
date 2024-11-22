c97d749971cd3b997d5afdf2396f6be8f2d9527b
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-11-22 14:41:24 +0000

    Stable

--- src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/web/controller/CalculatorController.java
+++ src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/web/controller/CalculatorController.java
@@ -1,8 +1,5 @@
 package com.igorpetrovcm.neoloan.calculator.web.controller;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 import com.igorpetrovcm.neoloan.calculator.api.CalculatorApi;
@@ -12,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 
 import com.igorpetrovcm.neoloan.calculator.model.LoanOfferDTO;
 import com.igorpetrovcm.neoloan.calculator.model.LoanStatementRequestDTO;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,21 +27,4 @@ public class CalculatorController implements CalculatorApi {
                 HttpStatus.CREATED
         );
     }
-
-//    @GetMapping("/create")
-//    public ResponseEntity<LoanStatementRequestDTO> createTestRequestGet(){
-//        LoanStatementRequestDTO loanStatement =
-//                LoanStatementRequestDTO.builder()
-//                        .amount(BigDecimal.valueOf(20000))
-//                        .firstName("Hell")
-//                        .lastName("Hell")
-//                        .middleName(null)
-//                        .email("ieefs33@mail.ru")
-//                        .birthdate(LocalDate.of(2000, 10, 10))
-//                        .passportSeries(BigDecimal.valueOf(1234))
-//                        .passportNumber(BigDecimal.valueOf(123456))
-//                        .build();
-//
-//        return ResponseEntity.ok(loanStatement);
-//    }
 }
