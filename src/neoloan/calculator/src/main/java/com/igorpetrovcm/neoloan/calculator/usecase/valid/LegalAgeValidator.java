f3f88585dba8826b8b79323191c471831a378591
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-11-24 17:28:41 +0000

    Current stable version

--- src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/usecase/valid/LegalAgeValidator.java
+++ src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/usecase/valid/LegalAgeValidator.java
@@ -1,17 +1,18 @@
 package com.igorpetrovcm.neoloan.calculator.usecase.valid;
 
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
+import com.igorpetrovcm.neoloan.calculator.model.LoanOfferDTO;
+import com.igorpetrovcm.neoloan.calculator.model.LoanStatementRequestDTO;
+import jakarta.validation.*;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 public class LegalAgeValidator implements
-        ConstraintValidator<LegalAgeConstraint, LocalDate> {
+    ConstraintValidator<LegalAgeConstraint, LoanStatementRequestDTO>{
 
     @Override
-    public boolean isValid(LocalDate date, ConstraintValidatorContext context){
-        Period period = Period.between(date, LocalDate.now());
+    public boolean isValid(LoanStatementRequestDTO statement, ConstraintValidatorContext context){
+        Period period = Period.between(statement.getBirthdate(), LocalDate.now());
 
         return (period.getYears() - 18) >= 0;
     }
--- src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/web/controller/CalculatorController.java
+++ src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/web/controller/CalculatorController.java
@@ -1,5 +1,7 @@
 package com.igorpetrovcm.neoloan.calculator.web.controller;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 
 import com.igorpetrovcm.neoloan.calculator.api.CalculatorApi;
@@ -7,8 +9,10 @@ import com.igorpetrovcm.neoloan.calculator.model.LoanOfferDTO;
 import com.igorpetrovcm.neoloan.calculator.usecase.CreateLoanOffers;
 import com.igorpetrovcm.neoloan.calculator.model.LoanStatementRequestDTO;
 
+import org.springframework.core.MethodParameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,6 +25,11 @@ public class CalculatorController implements CalculatorApi {
 
     @Override
     public ResponseEntity<List<LoanOfferDTO>> calculatorOffersPost(LoanStatementRequestDTO loanStatement){
+        Period periodFromBirth = Period.between(loanStatement.getBirthdate(), LocalDate.now());
+        if (periodFromBirth.getYears() - 18 < 0){
+            throw new IllegalArgumentException("date of birth");
+        }
+
         return new ResponseEntity<>(
                 creatorLoanOffers.createOffers(loanStatement),
                 HttpStatus.CREATED
