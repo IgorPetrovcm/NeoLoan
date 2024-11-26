a97837420ec36ac45fe8f4ddcdc16106604af04f
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-11-26 21:39:17 +0000

    Add the message not readable exception handler

--- src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/web/exception/ControllerExceptionHandler.java
+++ src/neoloan/calculator/src/main/java/com/igorpetrovcm/neoloan/calculator/web/exception/ControllerExceptionHandler.java
@@ -2,11 +2,13 @@ package com.igorpetrovcm.neoloan.calculator.web.exception;
 
 
 import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Objects;
@@ -20,13 +22,39 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
             HttpHeaders headers,
             HttpStatusCode status,
             WebRequest request
-    ) {
+    ){
         ProblemDetail problemDetail = handleValidationException(ex);
         return ResponseEntity.status(status.value()).body(problemDetail);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(
+            HttpMessageNotReadableException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ){
+        ProblemDetail problemDetail = handleHttpMessageNotReadableException(ex, status, request);
+        return ResponseEntity.status(status.value()).body(problemDetail);
+    }
+
+    private ProblemDetail handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException ex,
+            HttpStatusCode statusCode,
+            WebRequest request
+    ){
+        String details = ex.getMessage();
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(statusCode, details);
+        problemDetail.setTitle("Bad request");
+        problemDetail.setInstance(URI.create(request.getContextPath()));
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
     private ProblemDetail handleValidationException(MethodArgumentNotValidException ex) {
         String details = getErrorsDetails(ex);
+
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(ex.getStatusCode(), details);
         problemDetail.setTitle("Bad request");
         problemDetail.setInstance(ex.getBody().getInstance());
